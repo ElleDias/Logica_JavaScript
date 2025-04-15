44a89af147f4a7485f3ace212e03a485883e272e
Danielle Dias <danielle.c.santos9@aluno.senai.br>
2025-04-15 16:48:00 -0300

    exercicios de javascript

--- Exercicios/Ex3.js
+++ Exercicios/Ex3.js
@@ -3,9 +3,6 @@
 // começam com essa letra (ex: 'a'). Use split(), startsWith() e filter().
 
 function contarPalavrasComLetra(frase, letra) {
-    const palavras = frase.split(' ');
-    return palavras.filter(palavra => palavra.toLowerCase().startsWith(letra.toLowerCase())).length;
   }
-  
-  console.log(contarPalavrasComLetra('Amazônia é a maior floresta do mundo', 'a'));
+
   
--- Exercicios/Ex7.js
+++ Exercicios/Ex7.js
@@ -3,7 +3,6 @@
 //numéricos ele contém usando split() e filter() com isNaN().
 
 function contarDigitos(texto) {
-    return texto.split('').filter(caractere => !isNaN(caractere) && caractere !== ' ').length;
   }
-  console.log(contarDigitos('A senha é 12345 e tem 6 caracteres!'));
+ 
   
