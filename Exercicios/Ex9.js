b4cccdac324752eb003896cbeceee8afc4b25da5
Danielle Dias <danielle.c.santos9@aluno.senai.br>
2025-04-15 15:39:58 -0300

    Subindo os desafios de javascript

---
+++ Exercicios/Ex9.js
@@ -0,0 +1,12 @@
+//9. Verificar Palíndromo com Funções
+//Crie uma função que verifica se uma palavra
+//é palíndroma usando split(), reverse() e join().
+
+function verificarPalindromo(palavra) {
+    const palavraReversa = palavra.split('').reverse().join('');
+    return palavra === palavraReversa;
+  }
+  
+  console.log(verificarPalindromo('arara')); // Saída: true
+  console.log(verificarPalindromo('olá')); // Saída: false
+  
